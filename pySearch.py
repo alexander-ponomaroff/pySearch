0a410d61ee6f890f305f5822d7d44cdbe23cd858
Alexander Ponomaroff <a.ponomaroff@gmail.com>
2018-11-16 15:25:55 -0500

    Increased search engine coverage

--- pySearch.py
+++ pySearch.py
@@ -12,7 +12,7 @@ argparser.add_argument("-d", "--domain", help="Changes the domain extention", na
 args = argparser.parse_args()
 
 class Search:
-    def __init__(self, searchIn = None, engineIn = "google", domainIn = "ca"):
+    def __init__(self, searchIn = None, engineIn = "yahoo", domainIn = "com"):
         self.searchRaw = searchIn
         self.searchQuery = ""
         self.engine = engineIn
@@ -30,22 +30,48 @@ class Search:
     def setdomain(self, domainIn):
         self.domain = domainIn
     #end of setdomain
-
+        
     def buildLink(self):
+        self.buildQuery()
+        self.url = "http://www." + self.engine + "." + self.domain + self.searchString + self.searchQuery
+    #end of link building
+
+    def openBrowser(self):
+        webbrowser.open_new_tab(self.url)        
+    #end of openBrowser()
+    
+    def buildQuery(self):
         if self.engine == "amazon":
             self.searchString = "/s/keywords="
             self.searchQuery = "%20".join(self.searchRaw)
         elif self.engine == "twitter":
             self.searchQuery = " ".join(self.searchRaw)
+        elif self.engine == "yandex":
+            self.searchString = "/search/?text="
+            self.searchQuery = "+".join(self.searchRaw)
+        elif self.engine == "duckduckgo":
+            self.searchString = "/?q="
+            self.searchQuery = "+".join(self.searchRaw)
+        elif self.engine == "archive":
+            self.searchString = "/search.php?query="
+            self.searchQuery = "+".join(self.searchRaw)
+            self.domain = "org"
+        elif self.engine == "boardreader":
+            self.searchString = "/s/"
+            self.searchQuery = "+".join(self.searchRaw)+".html"
+        elif self.engine == "ask":
+            self.searchString = "/web?q="
+            self.searchQuery = "+".join(self.searchRaw)
+        elif self.engine == "facebook":
+            self.searchString = "/search/str/"
+            self.searchQuery = "+".join(self.searchRaw)+"/keywords_search"
+        elif self.engine == "yahoo":
+            self.searchString = "/search?p="
+            self.searchQuery = "+".join(self.searchRaw)
+            self.engine = "search.yahoo"
         else:
             self.searchQuery = "+".join(self.searchRaw)
         #end of search exceptions
-        self.url = "http://www." + self.engine + "." + self.domain + self.searchString + self.searchQuery
-    #end of link building
-
-    def openBrowser(self):
-        webbrowser.open_new_tab(self.url)
-    #end of openBrowser()
 #end of Query
 
 searchObj = Search(args.s)
